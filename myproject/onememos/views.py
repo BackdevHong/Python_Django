a1a6031e688081829aab72b99bfa6c3ccb14420a
BackdevHong <ghddls0704@kakao.com>
2022-09-26 01:17:36 +0900

    hello world

--- myproject/onememos/views.py
+++ myproject/onememos/views.py
@@ -1,8 +1,7 @@
-from django.http import HTTPResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
-
 def index(req):
-    return HTTPResponse("Onememos~ Hello, World~^.~")
+    return HttpResponse("Onememos~ Hello, World~^.~")
